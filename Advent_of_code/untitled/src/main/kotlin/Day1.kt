12acf56c9331aee3998ea01d0e50657ac58a8508
Arjen Alkema <arjenalkema@outlook.com>
2023-12-01 19:42:51 +0100

    day 1 part 2

--- Advent_of_code/untitled/src/main/kotlin/Day1.kt
+++ Advent_of_code/untitled/src/main/kotlin/Day1.kt
@@ -3,21 +3,20 @@ import java.nio.charset.Charset
 
 class Day1 {
 
-    private val nums = listOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-    private val numsSpelled = listOf("one", "two", "three" , "four", "five", "six" , "seven", "eight", "nine")
+    private val nums = (0..9).map { it.toString() }
+    private val numsSpelled = listOf("one", "two", "three" , "four", "five", "six", "seven", "eight", "nine")
     private val data = parse()
 
     private fun parse(): List<String> {
-        return File("/Users/aalkema/Private/Advent_of_code/untitled/src/main/resources/Data-1A.txt").readLines(Charset.defaultCharset())
+        return File("src/main/resources/Data-1A.txt").readLines(Charset.defaultCharset())
     }
 
     fun calculatePart1(): Number {
         var sum = 0
         data.forEach { line ->
-            val numsInLine = line.filter { c -> nums.contains(c) }
-            val firstNum = numsInLine.first()
-            val lastNum = numsInLine.last()
-            val number = firstNum.toString() + lastNum.toString()
+            val firstNum = line.findAnyOf(nums)?.second
+            val lastNum = line.findLastAnyOf(nums)?.second
+            val number = firstNum + lastNum
             sum += number.toInt()
         }
         return sum
@@ -25,11 +24,35 @@ class Day1 {
 
     fun calculatePart2(): Number {
         var sum = 0
-        data.forEach {line ->
-            line.forEachIndexed { index, c ->
+        data.forEach { line ->
+            val (indexSpelledFirst: Int?, firstNumSpelled: String?) = line.findAnyOf(numsSpelled) ?: (null to null)
+            val (indexNumsFirst: Int?, firstNums: String?) = line.findAnyOf(nums) ?: (null to null)
+            val firstNum = indexSpelledFirst?.let {
+                if (indexNumsFirst != null && indexNumsFirst < indexSpelledFirst) firstNums!!.toInt() else firstNumSpelled!!.convertSpelledNumToInt()
+            } ?: firstNums!!.toInt()
 
-            }
+            val (indexSpelledLast: Int?, lastNumSpelled: String?) = line.findLastAnyOf(numsSpelled) ?: (null to null)
+            val (indexNumslast: Int?, lastNums: String?) = line.findLastAnyOf(nums) ?: (null to null)
+            val lastNum = indexSpelledLast?.let {
+                if (indexNumslast != null && indexNumslast > indexSpelledLast) lastNums!!.toInt() else lastNumSpelled!!.convertSpelledNumToInt()
+            } ?: lastNums!!.toInt()
+
+            val combinedNum = firstNum.toString() + lastNum.toString()
+            sum += combinedNum.toInt()
         }
         return sum
+    }
+
+    fun String.convertSpelledNumToInt(): Int = when (this) {
+        "one" -> 1
+        "two" -> 2
+        "three" -> 3
+        "four" -> 4
+        "five" -> 5
+        "six" -> 6
+        "seven" -> 7
+        "eight" -> 8
+        "nine" -> 9
+        else -> throw Exception("Bruh")
     }
 }
--- Advent_of_code/untitled/src/main/kotlin/Main.kt
+++ Advent_of_code/untitled/src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
-fun main(args: Array<String>) {
+fun main() {
     val day1 = Day1()
     println(day1.calculatePart1())
+    println(day1.calculatePart2())
 }

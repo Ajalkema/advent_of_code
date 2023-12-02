998a46a07967217c4dc0b924976a4c059685c06f
Arjen Alkema <arjenalkema@outlook.com>
2023-12-02 10:41:17 +0100

    clean up

--- Advent_of_code/untitled/src/main/kotlin/Day02.kt
+++ Advent_of_code/untitled/src/main/kotlin/Day02.kt
@@ -3,12 +3,12 @@ class Day02 {
     private val colors = mapOf("red" to 12, "green" to 13, "blue" to 14)
 
     fun part1(input: List<String>): Int {
-        val splittedLine = input.map { it.split(" ").map { it.replace(Regex("""[:;,]"""), "") } }
+        val splittedLine = input.map { it.split(" ").map { it.replace(Regex("[:;,]"), "") } }
         return splittedLine.filter { gamePossible(it) }.sumOf { it[1].toInt() }
     }
 
     fun part2(input: List<String>): Int {
-        val splittedLine = input.map { it.split(" ").map { it.replace(Regex("""[:;,]"""), "") } }
+        val splittedLine = input.map { it.split(" ").map { it.replace(Regex("[:;,]"), "") } }
         return splittedLine.sumOf { minimumCubesSquared(it) }
     }
 
@@ -23,7 +23,7 @@ class Day02 {
         val minimumCubesMap = mutableMapOf("red" to 0, "green" to 0, "blue" to 0)
         colors.keys.forEach { color ->
             line.forEachIndexed { i, s ->
-                if (s.toIntOrNull() != null && line[i + 1].equals(color) && minimumCubesMap[color]!! < s.toInt()) minimumCubesMap[color] = s.toInt()
+                if (s.toIntOrNull() != null && line[i + 1] == color && minimumCubesMap[color]!! < s.toInt()) minimumCubesMap[color] = s.toInt()
             }
         }
         return minimumCubesMap.values.reduce(Int::times)

16f753a8845249fab14971f39efbf10b1231c17e
Arjen Alkema <arjenalkema@outlook.com>
2023-12-03 20:08:14 +0100

    part 1

--- Advent_of_code/untitled/src/main/kotlin/Day03.kt
+++ Advent_of_code/untitled/src/main/kotlin/Day03.kt
@@ -3,49 +3,30 @@ import kotlin.math.min
 
 class Day03(val input: List<String>) {
 
+    private val numbers = Regex("\\d+")
     private val symbols = Regex("[^\\d.]")
+    private val stars = Regex("[*]")
+    private val lineLength = input[0].length
 
     fun part1(): Int {
-        val numbersMap = mutableMapOf<Pair<Int, Int>, Int>()
-        input.forEachIndexed { lineIndex, line ->
-            line.forEachIndexed { charIndex, _ ->
-                if (charIndex > line.length - 2) return@forEachIndexed
-                if (charIndex > 0) {
-                    if (line.subSequence(charIndex, min(charIndex + 3, line.length)).toString().toIntOrNull() != null && !line[charIndex-1].isDigit()) {
-                        numbersMap[charIndex to lineIndex] = line.subSequence(charIndex, min(charIndex + 3, line.length)).toString().toIntOrNull() ?: 0
-                    }
-                    if (charIndex < line.length - 2) {
-                        if (line.subSequence(charIndex, min(charIndex + 2, line.length)).toString().toIntOrNull() != null
-                            && !line[charIndex - 1].isDigit()
-                            && !line[charIndex + 2].isDigit()) {
-                                numbersMap[charIndex to lineIndex] = line.subSequence(charIndex, min(charIndex + 2, line.length)).toString().toIntOrNull() ?: 0
-                        }
-                    }
-                } else {
-                    if (line.subSequence(charIndex, min(charIndex + 3, line.length)).toString().toIntOrNull() != null) {
-                        numbersMap[charIndex to lineIndex] = line.subSequence(charIndex, min(charIndex + 3, line.length)).toString().toIntOrNull() ?: 0
-                    }
-                    if (line.subSequence(charIndex, min(charIndex + 2, line.length)).toString().toIntOrNull() != null && !line[charIndex+2].isDigit()) {
-                        numbersMap[charIndex to lineIndex] = line.subSequence(charIndex, min(charIndex + 2, line.length)).toString().toIntOrNull() ?: 0
-                    }
-                }
-            }
-        }
+        val numbersMap = input.map { line -> numbers.findAll(line).map { it.value to it.range } }
+        numbersMap.forEach { it.println() }
 
         var sum = 0
-        numbersMap.keys.forEach {
-            val (charIndex, lineIndex) = it
-            if (hasSymbolsNearby(input[lineIndex], charIndex, lineIndex)) {
-                sum += numbersMap[charIndex to lineIndex]!!
+        numbersMap.forEachIndexed { index, line ->
+            line.forEach { numberMap ->
+                if (hasSymbolsNearby(numberMap, index)) {
+                    sum += numberMap.first.toInt()
+                }
             }
         }
         return sum
     }
 
-    fun hasSymbolsNearby(line: String, charIndex: Int, lineIndex: Int): Boolean {
-        if (input[lineIndex].subSequence(max(charIndex - 1, 0), min(charIndex + 4, line.length)).contains(symbols)) return true
-        if (input[max(lineIndex - 1, 0)].subSequence(max(charIndex - 1, 0), min(charIndex + 4, line.length)).contains(symbols)) return true
-        if (input[min(lineIndex + 1, input.size -1)].subSequence(max(charIndex - 1, 0), min(charIndex + 4, line.length)).contains(symbols)) return true
+    fun hasSymbolsNearby(numberMap: Pair<String, IntRange>, lineIndex: Int): Boolean {
+        if (input[lineIndex].subSequence(max(numberMap.second.first - 1, 0), min(numberMap.second.last + 2, lineLength)).contains(symbols)) return true
+        if (input[max(lineIndex - 1, 0)].subSequence(max(numberMap.second.first - 1, 0), min(numberMap.second.last + 2, lineLength)).contains(symbols)) return true
+        if (input[min(lineIndex + 1, input.size -1)].subSequence(max(numberMap.second.first - 1, 0), min(numberMap.second.last + 2, lineLength)).contains(symbols)) return true
         return false
     }
 }
--- Advent_of_code/untitled/src/main/kotlin/Main.kt
+++ Advent_of_code/untitled/src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val day03 = Day03(readInput("Test_data_3"))
-//    val day03 = Day03(readInput("Data_3"))
+//    val day03 = Day03(readInput("Test_data_3"))
+    val day03 = Day03(readInput("Data_3"))
     day03.part1().println()
 }

4949d76be4923e72aed5b4f5f5597bc67a23db10
Arjen Alkema <arjenalkema@outlook.com>
2023-12-04 07:10:33 +0100

    part 2

--- Advent_of_code/untitled/src/main/kotlin/Day03.kt
+++ Advent_of_code/untitled/src/main/kotlin/Day03.kt
@@ -7,11 +7,9 @@ class Day03(val input: List<String>) {
     private val symbols = Regex("[^\\d.]")
     private val stars = Regex("[*]")
     private val lineLength = input[0].length
+    private val numbersMap = input.map { line -> numbers.findAll(line).map { it.value to it.range } }
 
     fun part1(): Int {
-        val numbersMap = input.map { line -> numbers.findAll(line).map { it.value to it.range } }
-        numbersMap.forEach { it.println() }
-
         var sum = 0
         numbersMap.forEachIndexed { index, line ->
             line.forEach { numberMap ->
@@ -23,10 +21,43 @@ class Day03(val input: List<String>) {
         return sum
     }
 
-    fun hasSymbolsNearby(numberMap: Pair<String, IntRange>, lineIndex: Int): Boolean {
+    fun part2(): Int {
+        val starIndex = input.map { line -> stars.findAll(line).map { it.range } }
+
+        var sum = 0
+        starIndex.forEachIndexed { lineIndexStar, line ->
+            line.forEach { starIndex ->
+                sum += twoNumbersMultipliedIfPossible(starIndex, lineIndexStar)
+            }
+        }
+        return sum
+    }
+
+    private fun hasSymbolsNearby(numberMap: Pair<String, IntRange>, lineIndex: Int): Boolean {
         if (input[lineIndex].subSequence(max(numberMap.second.first - 1, 0), min(numberMap.second.last + 2, lineLength)).contains(symbols)) return true
         if (input[max(lineIndex - 1, 0)].subSequence(max(numberMap.second.first - 1, 0), min(numberMap.second.last + 2, lineLength)).contains(symbols)) return true
         if (input[min(lineIndex + 1, input.size -1)].subSequence(max(numberMap.second.first - 1, 0), min(numberMap.second.last + 2, lineLength)).contains(symbols)) return true
         return false
+    }
+
+    private fun twoNumbersMultipliedIfPossible(starIndex: IntRange, lineIndexStar: Int): Int {
+        val numsCloseToStar = mutableListOf<Int>()
+        numbersMap.forEachIndexed { lineIndex, line ->
+            line.forEach {
+                if (hasStarNearby(it.second, lineIndex, starIndex.first, lineIndexStar)) {
+                    numsCloseToStar.add(it.first.toInt())
+                }
+            }
+        }
+        if (numsCloseToStar.size == 2) {
+            return numsCloseToStar.reduce(Int::times)
+        }
+        return 0
+    }
+
+    private fun hasStarNearby(numRange: IntRange, numLineIndex: Int, starRange: Int, lineIndexStar: Int): Boolean {
+        if (numLineIndex !in lineIndexStar-1..lineIndexStar+1) return false
+        if (starRange !in (numRange.first-1..numRange.last+1)) return false
+        return true
     }
 }
--- Advent_of_code/untitled/src/main/kotlin/Main.kt
+++ Advent_of_code/untitled/src/main/kotlin/Main.kt
@@ -2,4 +2,5 @@ fun main() {
 //    val day03 = Day03(readInput("Test_data_3"))
     val day03 = Day03(readInput("Data_3"))
     day03.part1().println()
+    day03.part2().println()
 }

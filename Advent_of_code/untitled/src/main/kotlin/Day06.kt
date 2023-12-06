4759c9ad294ba5216cc56f0024b3e8c8b6be079c
Arjen Alkema <arjen.alkema@triodos.com>
2023-12-06 15:58:53 +0100

    day 6 part 2 + refactor

--- Advent_of_code/untitled/src/main/kotlin/Day06.kt
+++ Advent_of_code/untitled/src/main/kotlin/Day06.kt
@@ -1,21 +1,34 @@
 class Day06(input: List<String>) {
 
-    private val times = input[0].split(" ").mapNotNull { it.toIntOrNull() }
-    private val distances = input[1].split(" ").mapNotNull { it.toIntOrNull() }
+    private val times = input[0].split(" ").mapNotNull { it.toLongOrNull() }
+    private val distances = input[1].split(" ").mapNotNull { it.toLongOrNull() }
 
-    fun part1(): Int {
-        val result = MutableList(times.size) { 0 }
+    fun part1(): Long {
+        val result = MutableList(times.size) { 0L }
         times.forEachIndexed { i, time ->
-            val indexOfFirstViable = (1..<time).indexOfFirst { wait ->
-                distanceTravelled(time, wait) > distances[i]
-            }
-            result[i] = time.floorDiv(2).minus(indexOfFirstViable).times(2)
-            if (time % 2 == 0) result[i]--
+            result[i] = calculateResult(time, distances[i])
         }
-        return result.reduce(Int::times)
+        return result.reduce(Long::times)
     }
 
-    private fun distanceTravelled(time: Int, wait: Int): Int {
+    fun part2(): Long {
+        val combinedTime = times.joinToString("") { it.toString() }.toLong()
+        val combinedDistance = distances.joinToString("") { it.toString() }.toLong()
+
+        return calculateResult(combinedTime, combinedDistance)
+    }
+
+    private fun calculateResult(time: Long, distance: Long): Long {
+        val indexOfFirstViable = (1..<time).indexOfFirst { wait ->
+            distanceTravelled(time, wait) > distance
+        }
+        var result = time.floorDiv(2).minus(indexOfFirstViable).times(2)
+        if (time % 2 == 0L) result--
+
+        return result
+    }
+
+    private fun distanceTravelled(time: Long, wait: Long): Long {
         return (time-wait).times(wait)
     }
 }
--- Advent_of_code/untitled/src/main/kotlin/Main.kt
+++ Advent_of_code/untitled/src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 fun main() {
     val day06 = Day06(readInput("Data_6"))
     day06.part1().println()
+    day06.part2().println()
 }

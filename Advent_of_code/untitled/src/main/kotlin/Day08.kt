aa96b7793c741dfe7565efec20514dd198a222fe
Arjen Alkema <arjenalkema@outlook.com>
2023-12-09 15:37:45 +0100

    day 8 part 2

--- Advent_of_code/untitled/src/main/kotlin/Day08.kt
+++ Advent_of_code/untitled/src/main/kotlin/Day08.kt
@@ -14,12 +14,54 @@ class Day08(input: List<String>) {
         while (currentLocation != "ZZZ") {
             if (instructions[steps.mod(BigInteger.valueOf(instructions.size.toLong())).toInt()] == 'L') {
                 currentLocation = directionsMap[currentLocation]!!.first
-                steps++
             } else {
                 currentLocation = directionsMap[currentLocation]!!.second
-                steps++
             }
+            steps++
         }
         return steps
+    }
+
+    fun part2(): BigInteger {
+        val currentLocations = directionsMap.keys.filter { it.endsWith("A") }.toMutableList()
+        val lol = currentLocations.map { location -> stepsToFirstViableNode(location) }
+        return findLCMOfListOfNumbers(lol)
+    }
+
+    private fun stepsToFirstViableNode(start: String): BigInteger {
+        var steps = BigInteger.ZERO
+        var currentLocation = start
+
+        while (!currentLocation.endsWith("Z")) {
+            if (instructions[steps.mod(BigInteger.valueOf(instructions.size.toLong())).toInt()] == 'L') {
+                currentLocation = directionsMap[currentLocation]!!.first
+            } else {
+                currentLocation = directionsMap[currentLocation]!!.second
+            }
+            steps++
+        }
+        return steps
+    }
+
+    private fun findLCM(a: BigInteger, b: BigInteger): BigInteger {
+        val larger = if (a > b) a else b
+        val maxLcm = a * b
+        var lcm = larger
+        while (lcm <= maxLcm) {
+            if (lcm.mod(a).toInt() == 0 && lcm.mod(b).toInt() == 0) {
+                return lcm
+            }
+            lcm += larger
+        }
+        return maxLcm
+    }
+
+
+    private fun findLCMOfListOfNumbers(numbers: List<BigInteger>): BigInteger {
+        var result = numbers[0]
+        for (i in 1 until numbers.size) {
+            result = findLCM(result, numbers[i])
+        }
+        return result
     }
 }
--- Advent_of_code/untitled/src/main/kotlin/Main.kt
+++ Advent_of_code/untitled/src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 fun main() {
     val day08 = Day08(readInput("Data_8"))
     day08.part1().println()
+    day08.part2().println()
 }

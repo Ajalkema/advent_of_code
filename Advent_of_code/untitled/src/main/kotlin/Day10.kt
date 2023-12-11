8ac4746a55c74f851d9f39c86285e6ec54991a48
Arjen Alkema <arjen.alkema@triodos.com>
2023-12-11 18:29:47 +0100

    draft day 10 part 2

--- Advent_of_code/untitled/src/main/kotlin/Day10.kt
+++ Advent_of_code/untitled/src/main/kotlin/Day10.kt
@@ -16,6 +16,34 @@ class Day10(val input: List<String>) {
         return count.floorDiv(2)
     }
 
+    fun part2(): Int {
+        var up = 1
+        var down = 0
+        var left = 0
+        var right = 1
+
+        var currentPosition = start
+        var previousPosition: Position? = null
+        do {
+            val tmp = currentPosition
+            currentPosition = findNextTile(currentPosition, previousPosition)
+            previousPosition = tmp
+            when (grid[currentPosition.x][currentPosition.y]) {
+                'L' -> { up++; right++ }
+                'F' -> { down++; right++ }
+                'J' -> { up++; left++ }
+                '7' -> { down++; left++ }
+                '|' -> { up++; down++ }
+                '-' -> { left++; right++ }
+            }
+        } while (currentPosition != start)
+        up.println()
+        down.println()
+        left.println()
+        right.println()
+        return 0
+    }
+
     private fun findNextTile(position: Position, previousPosition: Position?): Position {
         if (grid[position.x][position.y] in listOf('|','L', 'J', 'S')) {
             if (northAight(position) && previousPosition != position.copy(x = position.x - 1)) return position.copy(x = position.x - 1)
--- Advent_of_code/untitled/src/main/kotlin/Main.kt
+++ Advent_of_code/untitled/src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 fun main() {
-    val day10 = Day10(readInput("Data_10"))
+    val day10 = Day10(readInput("Test_data_10"))
     day10.part1().println()
+    day10.part2().println()
 }

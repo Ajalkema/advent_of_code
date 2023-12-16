18cff488d4796cfe71bcc3972305117f79f36812
Arjen Alkema <arjen.alkema@triodos.com>
2023-12-16 11:28:40 +0100

    you know

--- Advent_of_code/untitled/src/main/kotlin/Day10.kt
+++ Advent_of_code/untitled/src/main/kotlin/Day10.kt
@@ -31,9 +31,14 @@ class Day10(val input: List<String>) {
         grid.forEachIndexed { y, row ->
             row.forEachIndexed { x, _ ->
                 if (!paths.contains(Position(x, y))) {
+                    var lastCorner = 'S'
                     var intersections = 0
-                    ((x + 1)..<row.size).forEach lol@ {
+                    ((x + 1)..<row.size).forEach lol@{
+                        if (grid[y][it] == 'F' && lastCorner == 'J' && paths.contains(Position(it, y))) intersections++
+                        if (grid[y][it] == 'L' && lastCorner == '7' && paths.contains(Position(it, y))) intersections++
                         if (grid[y][it] == '|' && paths.contains(Position(it, y))) intersections++
+
+                        if (grid[y][it] in listOf('J', '7', 'F', 'L') && paths.contains(Position(it, y))) lastCorner = grid[y][it]
                     }
                     if (intersections % 2 != 0) count++
                 }
